f5cc56c585b07e3ef94e7053bc025adacd7f3b65
vandriodd <ludasaadi@gmail.com>
2023-12-02 13:17:18 -0300

    finish pda project core & some styling

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,13 +1,55 @@
+import "bulma/css/bulma.css";
 import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.jpg";
+import CortanaImage from "./images/cortana.jpg";
+import SiriImage from "./images/siri.jpg";
+
+// About the type of data of the images:
+// If our image is less than 9.7kb, then the image gets inlined,
+// that means the image is converted into a base64 string.
+// If the image is larger than 9.7kb, then
+// the image is treated and served up as a separate file by the development server
 
 // App component defines the props that it passes to ProfileCard
 function App() {
   return (
     <div>
-      <h1>Personal Digital Assistants</h1>
-      <ProfileCard title="Alexa" handle="@alexa99" />
-      <ProfileCard title="Cortana" handle="@cortana32" />
-      <ProfileCard title="Siri" handle="@siri01" />
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </section>
+
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa99"
+                imageSrc={AlexaImage}
+                description="Alexa was created by Amazon and helps you buy things."
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana32"
+                imageSrc={CortanaImage}
+                description="Cortana was made by Microsoft. Who knows what it does?"
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Siri"
+                handle="@siri01"
+                imageSrc={SiriImage}
+                description="Siri was made by Apple and is being phased out"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   )
 }
--- pdas/src/ProfileCard.js
+++ pdas/src/ProfileCard.js
@@ -1,16 +1,34 @@
 // React calls 3 times this function,
 // and each time it passes a different value for the props argument
-function ProfileCard({ title, handle }) {
-  // Destructuring props
+// Props -> data that can be passed from parent to a child component (unidirectional flow)
+// should use the same name for the prop and the variable that holds the value of the prop
+function ProfileCard({ title, handle, imageSrc, description }) {
+  // Destructuring props  
   // pull of a property from an obj and assign it to a variable
   // const { title, handle } = props;
   // or, instead, passes as argument an obj with the properties
+
+  // always debug the props with console.log()!
   return (
-    <div>
-      <h3>{title}</h3>
-      <p>{handle}</p>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={imageSrc} alt="pda logo" />
+        </figure>
+      </div>
+
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title is-4">{title}</p>
+          <p className="subtitle is-6">{handle}</p>
+        </div>
+        <div className="content">{description}</div>
+      </div>
     </div>
   )
 }
 
 export default ProfileCard;
+
+// In case of typo, the component prop will not render, but it will not throw an error
+// the prop will be undefined
