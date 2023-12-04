cea9a3cbcc0bd3ea5088547dca1bb8f3d5c45301
vandriodd <ludasaadi@gmail.com>
2023-12-04 16:37:02 -0300

    add api with axios (WIP)

--- pics/src/index.js
+++ pics/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+// import searchImages from './api';
+
+// testing porpuses
+// searchImages();
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
