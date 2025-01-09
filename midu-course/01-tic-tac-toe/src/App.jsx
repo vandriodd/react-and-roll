fcf21a0e46abeab445b5ca2407e2c7c597cc2a5d
vandriodd <ludasaadi@gmail.com>
2025-01-08 23:53:26 -0300

    feat: initial tic tac toe setup, draw table, init state and handle click on table

---
+++ midu-course/01-tic-tac-toe/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import "./App.css";
+
+const TURNS = {
+  X: "x",
+  O: "o",
+};
+
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? "is-selected" : ""}`;
+
+  const handleClick = () => {
+    updateBoard();
+  };
+
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  );
+};
+
+export default function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(TURNS.X);
+
+  const updateBoard = () => {
+    const nextTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+  };
+
+  return (
+    <main className="board">
+      <h1>Tic Tac Toe</h1>
+      <section className="game">
+        {board.map((_, index) => {
+          return (
+            <Square key={index} index={index}>
+              {board[index]}
+            </Square>
+          );
+        })}
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+      </section>
+    </main>
+  );
+}
