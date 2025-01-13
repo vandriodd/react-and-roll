57357d2a396f110fbfa04906b419adf4652ce403
vandriodd <ludasaadi@gmail.com>
2025-01-12 22:36:19 -0300

    feat: add board's logic, detect winner or tie, handle reset game

--- midu-course/01-tic-tac-toe/README.md
+++ midu-course/01-tic-tac-toe/README.md
@@ -1,1 +1,9 @@
 # Tic Tac Toe
+
+## Concepts
+
+### States in React
+
+They are asynchronous, this means that they do not block the execution of the code that comes right after. _Important to know for visual purposes._
+
+We also can pass a callback function as a second argument to `setState` to execute code after the state has been updated.
--- midu-course/01-tic-tac-toe/src/App.jsx
+++ midu-course/01-tic-tac-toe/src/App.jsx
@@ -10,7 +10,7 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
   const className = `square ${isSelected ? "is-selected" : ""}`;
 
   const handleClick = () => {
-    updateBoard();
+    updateBoard(index);
   };
 
   return (
@@ -20,21 +20,76 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
   );
 };
 
+const WINNER_COMBOS = [
+  [0, 1, 2], // Horizontal
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6], // Vertical
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8], // Diagonal
+  [2, 4, 6],
+];
+
 export default function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(TURNS.X);
+  const [winner, setWinner] = useState(null);
 
-  const updateBoard = () => {
+  const checkWinner = (board) => {
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo;
+
+      if (
+        board[a] &&
+        board[a] === board[b] &&
+        board[a] === board[c] // If the 3 squares are the same
+      ) {
+        return board[a]; // return winner
+      }
+    }
+    return null;
+  };
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(null);
+  };
+
+  const checkEndGame = (board) => {
+    // If there are no more null squares
+    return board.every((square) => square !== null);
+  };
+
+  const updateBoard = (index) => {
+    if (board[index] || winner) return;
+    const newBoard = [...board]; // Copy the board (superficially) -> RIGHT 👍🏼
+    // board[index] = turn -> WRONG 👎🏼
+    // We never should mutate the state or props directly, they are like inmutable
+    // If we mutate these values, React potentially won't be able to detect the changes and re-render the component
+    // The values of the new render ALWAYS should be new
+    newBoard[index] = turn;
+    setBoard(newBoard);
     const nextTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(nextTurn);
+
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      setWinner(newWinner);
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false); // tie
+    }
   };
 
   return (
     <main className="board">
       <h1>Tic Tac Toe</h1>
+      <button onClick={resetGame}>Reset</button>
       <section className="game">
         {board.map((_, index) => {
           return (
-            <Square key={index} index={index}>
+            <Square key={index} index={index} updateBoard={updateBoard}>
               {board[index]}
             </Square>
           );
@@ -44,6 +99,23 @@ export default function App() {
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
+      {winner !== null && (
+        <section className="winner">
+          <div className="text">
+            <h2>
+              {winner === false ? "It's a tie!" : `Player ${winner} wins!`}
+            </h2>
+
+            <header className="win">
+              {winner && <Square>{winner}</Square>}
+            </header>
+
+            <footer>
+              <button onClick={resetGame}>Restart</button>
+            </footer>
+          </div>
+        </section>
+      )}
     </main>
   );
 }
